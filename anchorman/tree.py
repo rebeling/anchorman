cc68247d4d512e537fc536a0ca9e8ea57e1c5a8d
Matthias Rebel <webmaster@rebeling.net>
2015-02-08 01:06:34 +0100

    Be robust on html input

--- anchorman/tree.py
+++ anchorman/tree.py
@@ -4,10 +4,12 @@
 from lxml import etree, html
 
 
-def to_tree(s):
-    xml = "<root> %s </root>" % s
-    return etree.fromstring(xml)
-
+def to_tree(string):
+    string = "<root>%s</root>" % string
+    try:
+        return etree.fromstring(string)
+    except:
+        return html.fromstring(string)
 
 def from_tree(tree):
     return etree.tounicode(tree)[6:-7].strip()
