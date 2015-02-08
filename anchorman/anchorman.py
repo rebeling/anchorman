c774b1e37a1dcd23c20f2b9c5d88c71abdd0887a
Matthias Rebel <webmaster@rebeling.net>
2015-02-08 01:06:01 +0100

    Improve module interface

--- anchorman/anchorman.py
+++ anchorman/anchorman.py
@@ -7,18 +7,23 @@ from linkit import remove_links
 from utils import sort_for_longest_match_first
 
 
+
+
 class Anchorman(object):
     """
     Create the main object for the add_links request.
     """
 
     def __init__(self, *args, **kwargs):
-        self.markup_format = {}
-        self.link_format = None
+        self.markup_format = {'tag': 'a','value_key': 'href'}
+        self.link_format = {}
         self.selector = './/a'
+        self.text = args[0]
+        self.links = args[1]
         self.result = None
         self.counts = None
         self.set_link_format(kwargs)
+        self.replaces_per_item=kwargs.get('replaces_per_item', None)
 
     def __str__(self):
         return self.result
@@ -49,14 +54,14 @@ class Anchorman(object):
             'case-sensitive': False,    # works for both, default is True
         }
         '''
-        markup_format = kwargs.get('markup_format', {})
-        if markup_format:
-            self.markup_format = markup_format
-        new_lf, selector = linker_format(markup_format)
+        markup_format = kwargs.get('markup_format', None)
+        mf = self.markup_format if markup_format == None else markup_format
+        self.markup_format = mf
+        new_lf, selector = linker_format(self.markup_format)
         self.link_format = new_lf if new_lf else self.link_format
         self.selector = selector if selector else self.selector
 
-    def add(self, links, text, **kwargs):
+    def add(self, *args, **kwargs): #links, text, **kwargs):
         '''
         augment text with the items of links
         links = [
@@ -76,13 +81,15 @@ class Anchorman(object):
             }, ...
         ]
         '''
+        if args:
+            self.text = args[0]
+            self.links = args[1]
         self.set_link_format(kwargs)
         kwargs['link_format'] = self.link_format
-        markup_format = kwargs.get('markup_format', {})
-        if markup_format:
-            self.markup_format = markup_format
-        links = sort_for_longest_match_first(links)
-        self.result, self.counts = add_links(text, links, **kwargs)
+        kwargs['markup_format'] = self.markup_format
+        links = sort_for_longest_match_first(kwargs.get('links', self.links))
+        kwargs['replaces_per_item'] = kwargs.get('replaces_per_item', self.replaces_per_item)
+        self.result, self.counts = add_links(self.text , links, **kwargs)
         return self.result
 
     def remove(self, *args, **kwargs):
@@ -90,76 +97,28 @@ class Anchorman(object):
         remove the markup driven by actual/latest markup_format
         '''
         self.set_link_format(kwargs)
-        text = args[0] if args else self.result
+        # text = args[0] if args else self.result
+        text = kwargs.get('text', self.result)
         self.result = remove_links(text,
             selector=self.selector,
             markup_format=self.markup_format)
 
 
+def add(text, links, **kwargs):
+    '''
+    call class on module level
+    '''
+    a = Anchorman(text, links, **kwargs)
+    a.add()
+    return a
+
 
 # if __name__ == '__main__':
 
-#     markup_format = {
-#         'tag': 'a',
-#         'value_key': 'href',
-#         'attributes': [
-#             ('style', 'color:blue;cursor:pointer;'),
-#             ('class', 'anchorman')
-#             ],
-#         'rm-identifier': 'anchorman-link',
-#         # 'highlighting': {
-#         #     'pre': '${{',
-#         #     'post': '}}'
-#         #     },
-#         # 'case-sensitive': False,
-#         }
+#     links = [{'fox': {'value': '/wiki/fox'}}, {'dog': {'value': '/wiki/dog'}}]
+#     text = "The quick brown fox jumps over the lazy <br> dog."
 
-#     links = [
-#             {'tail': {
-#                 'value': '/tail',
-#                 }
-#             },
-#             {'Fox': {
-#                 'value': '/fox',
-#                 }
-#             },
-#             {'mammals': {
-#                 'value': '/mammals',
-#                 }
-#             },
-#             {'red fox': {
-#                 'value': '/redfox',
-#                 'attributes': [
-#                     ('class', 'animal'),
-#                     ('style', 'font-size:23px;background:red'),
-#                     ('title', 'Fix und Foxi')
-#                     ]
-#                 }
-#             }
-#         ]
-
-#     string = (
-#         """
-#         <p>Foxes are small-to-medium-sized, omnivorous mammals belonging to several genera of the Canidae family. Foxes are slightly smaller than a medium-size domestic dog, with a flattened skull, upright triangular ears, a pointed, slightly upturned snout, and a long bushy tail (or brush).</p>
-#         <p>Twelve species belong to the monophyletic group of Vulpes genus of "true foxes". Approximately another 25 current or extinct species are always or sometimes called foxes; these foxes are either part of the paraphyletic group of the South American foxes and the outlying group, which consists of Bat-eared fox, Gray fox, and Island fox.[1] Foxes are found on every continent except Antarctica. By far the most common and widespread species of fox is the red fox (Vulpes vulpes) with about 47 recognized sub-species.[2] The global distribution of foxes, together with their widespread reputation for cunning, has contributed to their prominence in popular culture and folklore in many societies around the world. The hunting of foxes with packs of hounds, long an established pursuit in Europe, especially in the British Isles, was exported by European settlers to various parts of the New World.</p>"""
-#         )
-
-#     a = Anchorman()
-
-#     a.add(
-#         links,
-#         string,
-#         markup_format=markup_format, # default: a, href=value, class="anchorman"
-#         # replaces_per_item=1          # default: all occurences
-#         )
-
-#     print '\nenriched string'
+#     a = add(text, links)
+#     print a.result
+#     a.remove()
 #     print a
-#     print a.counts
-#     # print '\n### cleared version: ###'
-#     # a.remove()
-#     # print a
-
-#     page = "<html><head></head><body>%s</body></html>" % a
-#     with open('example.html', 'w') as f:
-#         f.write(page)
