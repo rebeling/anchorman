b6611434657d732a224d9670ee50c1160c053e4f
Matthias Rebel <webmaster@rebeling.net>
2015-04-10 09:41:59 +0200

    Reduce code

--- anchorman/utils.py
+++ anchorman/utils.py
@@ -2,13 +2,8 @@
 # -*- coding: utf-8 -*-
 
 def sort_longest_match_first(links):
-    keys = {x.keys()[0]:x for x in links}
-    xs = sorted(keys.keys(), key=len, reverse=True)
-    b = []
-    append = b.append
-    for y in xs:
-        append(keys[y])
-    return b
+    links.sort(key=lambda c: len(c.keys()[0]), reverse=True)
+    return links
 
 
 def validate_input(args, _isinstance=isinstance):
