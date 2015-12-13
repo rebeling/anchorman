6f2f12dfb98812238400cba6076f4895a001b520
Matthias Rebel <webmaster@rebeling.net>
2015-12-14 00:46:16 +0100

    Fix open file from setup

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 import yaml
+import os
+import sys
 
 config = yaml.load(open('etc/conf.yaml', 'r').read())
+
+cwd = os.getcwd()
+project_root = os.path.dirname(cwd)
+readme = open(project_root + '/anchorman/readme.rst', 'rt').read()
+requirements = open(project_root + '/anchorman/requirements.txt', 'rt').read()
 
 
 setup(
@@ -11,10 +18,10 @@ setup(
     author=config['author'],
     author_email=config.get('author_email'),
     url=config.get('url'),
-    description=open('readme.rst', 'rt').read(),
+    description=readme,
     packages=[config['name']],
     install_requires=[
         line.split('==')[0]
-        for line in open('requirements.txt', 'rt').read().split('\n')
+        for line in requirements.split('\n')
     ]
 )
