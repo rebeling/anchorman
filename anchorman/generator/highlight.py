e353869cd8749f5eb4ff6f7a71854ec7e8108697
Matthias Rebel <webmaster@rebeling.net>
2016-01-07 23:39:42 +0100

    Do not use format as pattern

--- anchorman/generator/highlight.py
+++ anchorman/generator/highlight.py
@@ -2,14 +2,17 @@
 from copy import copy
 
 
-def augment_highlight(highlight, item): #, highlight_markup):
-    """Fill the base highlight element with data of the item."""
+def augment_highlight(highlight, item):
+    """Fill the base highlight element with data of the item.
 
+    The string manipulation with format causes problems on templating
+    syntax with curly brackets.
+    """
     string = copy(highlight)
 
     # add the text
     item_key = item.keys()[0]
-    string = string.format(item_key)
+    string = string % (item_key)
 
     return string
 
@@ -17,6 +20,6 @@ def augment_highlight(highlight, item): #, highlight_markup):
 def create_highlight(highlight_markup):
     """Use format to create a base highlight element."""
 
-    highlight = highlight_markup['pre'] + "{}" + highlight_markup['post']
+    highlight = highlight_markup['pre'] + "%s" + highlight_markup['post']
 
     return highlight
