eec5b07d62cd2b667adfc83746a8d0c372575402
Matthias Rebel <webmaster@rebeling.net>
2016-03-13 23:27:09 +0100

    Augment parse yaml for testing

--- anchorman/configuration.py
+++ anchorman/configuration.py
@@ -1,17 +1,31 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
 import yaml
+from codecs import open
+
+
+def parse_yaml(filepath, loaded_from=__file__):
+    abspath = os.path.abspath(os.path.dirname(loaded_from))
+    path = "/".join([abspath, filepath])
+    try:
+        yaml_data = yaml.load(open(path, 'r', 'utf-8'))
+        return yaml_data
+    except IOError, e:
+        sys.stdout.write("No yaml file found: %s" % e)
+        raise IOError
+    except yaml.YAMLError, e:
+        sys.stdout.write("Error in configuration file: %s" % e)
+        raise yaml.YAMLError
 
 
 def get_config(project_conf=True):
     """Load default configuration."""
 
-    path = os.path.abspath(os.path.dirname(__file__))
-    print path
-    default = yaml.load(open("{0}/{1}".format(path, '../etc/link.yaml'), 'r').read())
+    default = parse_yaml("../etc/link.yaml")
 
     if project_conf:
-        conf = yaml.load(open("{0}/{1}".format(path, '../etc/conf.yaml'), 'r').read())
+        conf = parse_yaml("../etc/conf.yaml")
         default.update(conf)
 
     return default
